bd2c36cd7313e50561647e2cd1ca1fb1e8212668
adnanahmad07 <adnanahmad9313@gmail.com>
2024-01-17 10:41:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('nav').addClass("sticky");
+
+        } else {
+            $('nav').removeClass("sticky");
+        }
+
+    });
+});
+
+
+
+const menuBtn = document.getElementById("menu-btn");
+const menuCard = document.getElementById("menu-card");
+menuBtn.onclick = () => {
+    menuCard.style.right = "0px";
+}
+
+function closebtn() {
+    menuCard.style.right = "-400px";
+}
